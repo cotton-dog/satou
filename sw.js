17280ff5eff76205104590da70944eb0e3eaa766
cotton-dog <suzyben48@gmail.com>
2026-09-03 10:38:18 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,73 +1,62 @@
 const CACHE_NAME = "satou-v1";
 
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./lockscreen.html",
-    "./manifest.webmanifest"
+const FILES = [
+  "./",
+  "./index.html",
+  "./lockscreen.html",
+  "./manifest.webmanifest"
 ];
-
-
-/* =========================
-   安装
-========================= */
 
 self.addEventListener("install", event => {
 
-    event.waitUntil(
+  event.waitUntil(
 
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
 
-    );
+      return cache.addAll(FILES);
 
-    self.skipWaiting();
+    })
+
+  );
+
+  self.skipWaiting();
 
 });
 
-
-/* =========================
-   激活
-========================= */
 
 self.addEventListener("activate", event => {
 
-    event.waitUntil(
+  event.waitUntil(
 
-        caches.keys().then(keys =>
+    caches.keys().then(keys => {
 
-            Promise.all(
+      return Promise.all(
 
-                keys
-                .filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
 
-            )
+      );
 
-        )
+    })
 
-    );
+  );
 
-    self.clients.claim();
+  self.clients.claim();
 
 });
 
 
-/* =========================
-   请求
-========================= */
-
 self.addEventListener("fetch", event => {
 
-    event.respondWith(
+  event.respondWith(
 
-        caches.match(event.request)
-        .then(cached => {
+    caches.match(event.request).then(cached => {
 
-            return cached || fetch(event.request);
+      return cached || fetch(event.request);
 
-        })
+    })
 
-    );
+  );
 
 });
